9ee418aedea6f4e6352fb2136ea06a87c4ca3d09
Alisa <alisa.barsky@gmail.com>
2017-01-01 19:34:32 +0200

    sprite

--- app/src/main/java/com/example/alisa/myproject/GameView.java
+++ app/src/main/java/com/example/alisa/myproject/GameView.java
@@ -2,8 +2,12 @@ package com.example.alisa.myproject;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,6 +17,22 @@ import android.view.View;
  */
 
 public class GameView extends View {
+    private static final int NUM_FRAMES = 64;
+    private static final float X_SPEED = 28f;
+    Bitmap bgBitmap;
+    Character[]  ch;
+    RectF dst0 = new RectF();
+    RectF dst1 = new RectF();
+    Rect[] frames = new Rect[NUM_FRAMES];
+    int mCharHeight;
+    int mCharWidth;
+    int naiveFrameNam;
+    Bitmap Bitmapb1;
+    Bitmap Bitmapb2;
+    Bitmap Bitmapb3;
+    Paint topRectPaint = new Paint();
+    private int mViewHeight;
+    private int mViewWidth;
     public GameView(Context context) {
         super(context);
     }
@@ -30,4 +50,22 @@ public class GameView extends View {
         //Drawable drawable = res.getDrawable(R.drawable.bird1);
         //drawable.draw(canvas);
     }
+    private void prepareCharacter() {
+       Bitmap spritesBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.newbird_sprite);
+        // setup the rects
+        mCharWidth = spritesBitmap.getWidth() / 3;
+        mCharHeight = spritesBitmap.getHeight();
+
+        int i = 0; // rect index
+        for (int y = 0; y < 6; y++) { // row
+            for (int x = 0; x < 12; x++) { // column
+                frames[i] = new Rect(x * mCharWidth, y * mCharHeight, (x + 1) * mCharWidth, (y + 1) * mCharHeight);
+                i++;
+                if (i >= NUM_FRAMES) {
+                    break;
+                }
+            }
+        }
+    }
+
 }
