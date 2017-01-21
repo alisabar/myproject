84c5f88de12e138d4f400d09a70d7ca041df3b92
mik52023 <mik52023@walla.com>
2017-01-21 19:58:15 +0200

    added firebase

--- app/src/main/java/com/example/alisa/myproject/MainActivity.java
+++ app/src/main/java/com/example/alisa/myproject/MainActivity.java
@@ -6,15 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+
 import java.io.Console;
 import java.io.PrintStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 
 public void PlayButton(View view){
