861ee317591bd64106e244e7a5fa748f20e601fc
mik52023 <mik52023@walla.com>
2017-01-21 09:38:22 +0200

    added high score

--- app/src/main/java/com/example/alisa/myproject/GameOverActivity.java
+++ app/src/main/java/com/example/alisa/myproject/GameOverActivity.java
@@ -1,13 +1,19 @@
 package com.example.alisa.myproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class GameOverActivity extends Activity {
 
@@ -16,6 +22,13 @@ public class GameOverActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
 
+        TextView score=(TextView) findViewById(R.id.Score);
+        SharedPreferences sharedprep=getSharedPreferences("HighScore", Context.MODE_PRIVATE);
+
+        int high_m= sharedprep.getInt("minutes",0);
+        int high_s= sharedprep.getInt("seconds",0);
+
+        score.setText(String.valueOf(high_m)+ "."+String.valueOf(high_s));
     }
 
     public void goToMainMenu(View view) {
